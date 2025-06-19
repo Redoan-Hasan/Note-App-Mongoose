10b855bc889be0dc07fc90faff0f6a14fc6231c1
Redoan Hasan <redoanhasan57@gmail.com>
2025-06-20 02:05:55 +0600

    validation applied

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,12 +1,29 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
-
+import validator from "validator";
 const userSchema = new Schema<IUser>({
-    firstName: { type: String, required: true, trim: true , minlength: [2, 'First name must be at least 2 characters long, got {VALUE}'] },
-    lastName: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true , trim: true },
+    firstName: { type: String, required:[ true, 'First name is required' ], trim: true , minlength: [2, 'First name must be at least 2 characters long, got {VALUE}'] },
+    lastName: { type: String, required: true, trim: true, minlength:3,
+        validate:{
+            validator: function(v: string) {
+                return v.length >=3;
+            },
+            message: props => `Last name must be at least 3 characters long, got ${props.value}`
+        }
+     },
+    email: { type: String, required: true, unique: true , trim: true, 
+        validate: [validator.isEmail, 'Please provide a valid email address']
+    },
     password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+    role: { 
+        type: String,
+        // enum: ['user', 'admin'],
+        enum: {
+            values: ['user', 'admin'],
+            message: '{VALUE} is not a valid role'
+        },
+        default: 'user'
+    }
 }, {
     versionKey: false,
     timestamps: true
